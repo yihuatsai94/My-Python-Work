eabaac47258104dcdd177b832ea7c49c61e7694b
Cindy Tasi <jolin5809@gmail.com>
2019-12-31 15:32:50 +0800

    final

---
+++ eggs/spam.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+get_ipython().run_line_magic('matplotlib', 'inline')
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+n = randint(1,100)
+print(f"你今天的幸運數字是 {n}!")
